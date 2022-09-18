71213d9672fa54b661909e055be7af4ab168776d
Shop <mohamedrohiem597@gmail.com>
2022-09-18 10:41:49 +0200

    second commit

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -3,28 +3,46 @@ package com.example.currencyconverter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
-import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
-import kotlin.math.log
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity() {
     val TAG="MainActivity"
+    private val egyptionPound="Egyptian Pound"
+    private val americanDollar="American Dollar"
+    private val AED="AED"
+    private val GBP="GBP"
+    lateinit var fromDropDownMenu:AutoCompleteTextView
+    lateinit var toDropDownMenu: AutoCompleteTextView
+    lateinit var convertBtn:Button
+    lateinit var amountEt:TextInputEditText
+    lateinit var resultEt:TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeView()
+        populateDropDownMenue()
 
-
-        val convertBtn:Button=findViewById(R.id.convert_button)
-        val amountEt:TextInputEditText=findViewById(R.id.amount_edit_text)
-        val resultEt:TextInputEditText=findViewById(R.id.result_edit_text)
 
 
 
         convertBtn.setOnClickListener(){
             val amount=amountEt.text.toString().toDouble()
-            val result=amount.times(15.73)
+            val currencyToField=toDropDownMenu.text.toString()
+            Log.d(TAG,"TO $currencyToField")
+
+            var result:Double=0.0
+           result= when(currencyToField){
+                AED->BigDecimal.valueOf(amount.times(3.67)).setScale(2,RoundingMode.DOWN).toDouble()
+                egyptionPound->BigDecimal.valueOf(amount.times(19.26)).setScale(2,RoundingMode.DOWN).toDouble()
+                GBP->BigDecimal.valueOf(amount.times(0.87)).setScale(2,RoundingMode.DOWN).toDouble()
+               else->BigDecimal.valueOf(amount.times(1)).setScale(2,RoundingMode.DOWN).toDouble()
+            }
             resultEt.setText(result.toString())
             Log.d(TAG,"result = $result")
 
@@ -39,5 +57,20 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG,"Result: $comment")
             }*/
         }
+    }
+    private fun initializeView(){
+
+        convertBtn=findViewById(R.id.convert_button)
+        fromDropDownMenu=findViewById(R.id.from_auto_text_view)
+        toDropDownMenu=findViewById(R.id.to_edit_text)
+        amountEt=findViewById(R.id.amount_edit_text)
+        resultEt=findViewById(R.id.result_edit_text)
+    }
+    private  fun populateDropDownMenue(){
+        val listOfCountries= listOf(egyptionPound,americanDollar,AED,GBP)
+        //R resource folder
+        val adapter=ArrayAdapter(this,R.layout.drop_down_list,listOfCountries)
+        toDropDownMenu.setAdapter(adapter)
+        fromDropDownMenu.setAdapter(adapter)
     }
 }
